00727a0cc3b468bc410a1981998852ad603c155b
ianzSanchez <dian010205@gmail.com>
2025-03-27 20:43:54 +0700

    homepage done

--- app/src/main/java/com/example/myontire/MainActivity.java
+++ app/src/main/java/com/example/myontire/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myontire;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +21,17 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+
         });
+    // Intent ke activity berikutnya setelah 8 detik
+        new Handler().postDelayed(new Runnable() {
+        @Override
+        public void run() {
+            Intent intent = new Intent(MainActivity.this, homepage.class); //ganti homepage.class sama intent login
+            startActivity(intent);
+            finish();
+        }
+    }, 8000); // 8000 milidetik = 8 detik
     }
 }
